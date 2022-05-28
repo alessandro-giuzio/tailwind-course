684e3e664cc19221412931b5a0854c56368b8e58
alessandro-giuzio <giuzio@icloud.com>
2022-05-28 16:59:26 +0200

    first commit 🚀

---
+++ Login Modal/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    fontFamily: {
+      sans: ['Mulish', 'sans - serif'],
+      mono: ['Rokkitt', 'mono - space'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
